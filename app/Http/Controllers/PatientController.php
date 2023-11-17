ad0486e486326717c84678f6ca62984f96250656
MiqdamFerdiansyahHambali <miqd22166ti@student.nurulfikri.ac.id>
2023-11-17 13:31:47 +0700

    add:fitur: search by name & status

--- app/Http/Controllers/PatientController.php
+++ app/Http/Controllers/PatientController.php
@@ -7,14 +7,16 @@ use Illuminate\Http\Request;
 
 class PatientController extends Controller
 {
+
+    // Function get all data patients
     public function index()
     {
         $patients = Patient::all();
 
         if ($patients->isEmpty()) {
-            $response = ['data' => $patients];
+            $response = ['message' => "Data pattient is empty"];
 
-            return response()->json($response, 204);
+            return response()->json($response, 200);
         } else {
             $response = [
                 'message' => 'Data patients successfully loaded',
@@ -25,29 +27,29 @@ class PatientController extends Controller
         }
     }
 
-
+    // Function create data patients
     public function store(Request $request)
-    { {
-            $validateData = $request->validate([
-                'name' => 'required|max:50',
-                'phone' => 'required|string',
-                'address' => 'required|max:200',
-                'status' => 'required|max:50',
-                'in_date_at' => 'required|date',
-                'out_date_at' => 'nullable|date',
-            ]);
+    {
+        $validateData = $request->validate([
+            'name' => 'required|max:50',
+            'phone' => 'required|string',
+            'address' => 'required|max:200',
+            'status' => 'required|max:50',
+            'in_date_at' => 'required|date',
+            'out_date_at' => 'nullable|date',
+        ]);
 
-            $patient = Patient::create($validateData);
+        $patient = Patient::create($validateData);
 
-            $response = [
-                'message' => 'Data patient create successfully',
-                'data' => $patient,
-            ];
+        $response = [
+            'message' => 'Data patient create successfully',
+            'data' => $patient,
+        ];
 
-            return response()->json($response, 201);
-        }
+        return response()->json($response, 201);
     }
 
+    // Function show data patients by id
     public function show(string $id)
     {
         $patient = Patient::find($id);
@@ -68,6 +70,7 @@ class PatientController extends Controller
         }
     }
 
+    // Function update or edit data patients
     public function update(Request $request, string $id)
     {
         $patient = Patient::find($id);
@@ -105,6 +108,7 @@ class PatientController extends Controller
         }
     }
 
+    // Function delete data patients
     public function destroy(string $id)
     {
         $patient = Patient::find($id);
@@ -114,11 +118,101 @@ class PatientController extends Controller
 
             $response = ['message' => ' Data patient successfully deleted'];
 
-            return response()->json($response, 204);
+            return response()->json($response, 200);
         } else {
             $error = ['message' => 'Data not found, Deleted patient failed!!'];
 
             return response()->json($error, 404);
+        }
+    }
+
+    // Function search data patients by name
+    public function search(string $name)
+    {
+        $patients = Patient::where('name', 'like', '%' . $name . '%')->get();
+
+        if ($patients->count()) {
+            $response = [
+                'message' => 'Get search patients name successfully',
+                'data' => $patients,
+            ];
+            return response()->json($response, 200);
+        } else {
+            $error = ['message' => 'No patient found with the provided name'];
+
+            return response()->json($error, 404);
+        }
+    }
+
+    // Function get patients with positive status
+    public function positive()
+    {
+        $getPositive = Patient::where('status', 'positive')->get();
+        $totalPositive = $getPositive->count();
+
+        if ($totalPositive == 0) {
+            $response = [
+                'message' => 'Patients with positive status has empty!',
+                'total' => $totalPositive,
+            ];
+
+            return response()->json($response, 200);
+        } else {
+            $response = [
+                'message' => 'Get patients with positive status succesfully',
+                'total' => $totalPositive,
+                'data' => $getPositive,
+            ];
+
+            return response()->json($response, 200);
+        }
+    }
+
+    // Function get patients with recovered status
+    public function recovered()
+    {
+        $getRecovered = Patient::where('status', 'recovered')->get();
+        $totalRecovered = $getRecovered->count();
+
+        if ($totalRecovered == 0) {
+            $response = [
+                'message' => 'Patients with recovered status has empty!',
+                'total' => $totalRecovered,
+            ];
+
+            return response()->json($response, 200);
+        } else {
+            $response = [
+                'message' => 'Get patients with recovered status successfully',
+                'total' => $totalRecovered,
+                'data' => $getRecovered,
+            ];
+
+            return response()->json($response, 200);
+        }
+    }
+
+    // Function get patients with dead status
+    public function dead()
+    {
+        $getDead = Patient::where('status', 'dead')->get();
+        $totalDead = $getDead->count();
+
+        if ($totalDead == 0) {
+            $response = [
+                'message' => 'Patients with dead status has empty!',
+                'total' => $totalDead,
+            ];
+
+            return response()->json($response, 200);
+        } else {
+            $response = [
+                'message' => 'Get patients with dead status succesfully',
+                'total' => $totalDead,
+                'data' => $getDead,
+            ];
+
+            return response()->json($response, 200);
         }
     }
 }
--- routes/api.php
+++ routes/api.php
@@ -5,29 +5,24 @@ use App\Http\Controllers\PatientController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-/*
-|--------------------------------------------------------------------------
-| API Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register API routes for your application. These
-| routes are loaded by the RouteServiceProvider and all of them will
-| be assigned to the "api" middleware group. Make something great!
-|
-*/
 
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
 
+// Route login and register
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/register', [AuthController::class, 'register']);
 
-Route::get('/patients', [PatientController::class, 'index']);
-Route::post('/patients', [PatientController::class, 'store']);
-Route::get('/patients/{id}', [PatientController::class, 'show']);
-Route::put('/patients/{id}', [PatientController::class, 'update']);
-Route::delete('/patients/{id}', [PatientController::class, 'destroy']);
-
+// Group route patients with auth
 Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/patients', [PatientController::class, 'index']);
+    Route::post('/patients', [PatientController::class, 'store']);
+    Route::get('/patients/{id}', [PatientController::class, 'show']);
+    Route::put('/patients/{id}', [PatientController::class, 'update']);
+    Route::delete('/patients/{id}', [PatientController::class, 'destroy']);
+    Route::get('/patients/search/{name}', [PatientController::class, 'search']);
+    Route::get('/patients/status/positive', [PatientController::class, 'positive']);
+    Route::get('/patients/status/recovered', [PatientController::class, 'recovered']);
+    Route::get('/patients/status/dead', [PatientController::class, 'dead']);
 });
